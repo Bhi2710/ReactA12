c0a8d91bf5ff6b9f82d748b6b988b83993d83731
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2022-11-14 11:15:59 +0530

    Update slice.js

--- src/slice.js
+++ src/slice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const themeSlice = createSlice({
     name:"Theme",
     initialState:{
-        isLight:false,
+        isLight:true,
         msg:""
     },
     reducers:{
